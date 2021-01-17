aabcc2f6a26dbb2c4546aca334edca3b7a159d68
Terrence Kondou <kondou.terrence@gmail.com>
2021-01-17 18:07:23 +0000

    :tada: Initialize project with structure folder (Clean Architecture)

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3200;
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur le backend API");
+});
+app.listen(port, () => {
+  return console.log(`serveris listening on ${port}`);
+});
