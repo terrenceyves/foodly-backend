7da32dc3ebf0a1bad73f25aae212eab740c82cd0
Terrence Kondou <kondou.terrence@gmail.com>
2021-01-27 07:00:59 +0000

    Add Swagger dependency

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,46 @@
 import express from "express";
+// const swaggerJsDoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
 import "reflect-metadata";
+
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = 3200;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Customer API",
+      description: "Customer API Information",
+      contact: {
+        name: "Amazing Developer",
+      },
+      servers: ["http://localhost:3200"],
+    },
+  },
+  // ['.routes/*.js']
+  apis: ["build/server.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Use to request all
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.get("/", (req, res) => {
-  res.send("Bienvenue sur le backend API");
+  res.status(200).send("Bienvenue sur le backend API");
 });
+
 app.listen(port, () => {
-  return console.log(`serveris listening on ${port}`);
+  return console.log(`server is listening on ${port}`);
 });
